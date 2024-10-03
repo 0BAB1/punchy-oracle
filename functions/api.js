465a942bc5f62306749f4e28c0498d44dcc40727
BRH <62248962+0BAB1@users.noreply.github.com>
2024-10-03 16:35:19 +0200

    added 2 min time step

--- functions/api.js
+++ functions/api.js
@@ -1,27 +1,41 @@
 const MinaSigner = require('mina-signer');
 
+/**
+ * This file defines the API endpoint that shall return the current
+ * date with an adjusted 2 minutes time step. See note below.
+ */
+
 const signer = new MinaSigner({ network: 'testnet' });
 
 exports.handler = async function(event, context) {
-  const currentTime = Date.now();
-  
-  // Retrieve keys from environment variables
-  const publicKey = process.env.PUBLIC_KEY;
-  let privateKey = process.env.PRIVATE_KEY;
+    /**
+     * Regarding the response time & time step,
+     * We layed the hypothesis that the total response
+     * time is lower than and not equal to 2 minutes
+     * ie 120_000 ms. Which is an acceptable time step.
+     * This is due to the fact that the user has to know the time
+     * but the contract does not get the said time exactly when the
+     * user does, due to delays in server responses.
+     */
+    const currentTime = Math.floor(Date.now()/120000);
+    
+    // Retrieve keys from environment variables
+    const publicKey = process.env.PUBLIC_KEY;
+    let privateKey = process.env.PRIVATE_KEY;
 
-  // Create a signature
-  const dataToSign = { time: currentTime };
-  const signature = signer.signMessage(dataToSign, privateKey);
+    // Create a signature
+    const dataToSign = { time: currentTime };
+    const signature = signer.signMessage(dataToSign, privateKey);
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-        data: dataToSign,
-        signature: signature.signature,
-        publicKey: signature.publicKey,
-    })
-  };
+    return {
+        statusCode: 200,
+        headers: {
+        "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            data: dataToSign,
+            signature: signature.signature,
+            publicKey: signature.publicKey,
+        })
+    };
 };
--- readme.md
+++ readme.md
@@ -4,6 +4,12 @@ This simple oracle is used by the project smart contracts to retrieve the curren
 
 This small project was setup for easy deployment on netlify.
 
+Returns :
+
+- data {time : currentTime} with an adjusted 2 minute step
+- publicKey : the publicKey of the server
+- signature : time data signed using server's provate key to authenticate the oracle and approve the data
+
 # Test the API
 
 ```npm install```
